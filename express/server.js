10c06c0db39c86dfe091cc70494df2303c4cd63e
Karthikeyan NG <intrepidkarthi@gmail.com>
2020-02-22 13:16:22 +0530

    express app changes

--- express/server.js
+++ express/server.js
@@ -1,94 +1,77 @@
-var express = require('express');
-var app = express();
-var fs = require("fs");
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var multer  = require('multer');
-//var path = require('path');
 
-app.use(multer({ dest: '/tmp/'}).single('inputfilename'));
-app.use(cookieParser())
+var http = require("http");
+
+http.createServer(function (request, response) {
+    // Send the HTTP header 
+    // HTTP Status: 200 : OK
+    // Content Type: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    // Send the response body as "Hello World"
+    response.end('Hello World\n');
+ }).listen(8081);
+ // Console will print the message
+ console.log('Server running at http://127.0.0.1:8081/');
+var bodyParser = require('body-parser');
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+
+ var fs = require("fs");
+ var data = fs.readFileSync('input.txt');
+ console.log(data.toString());
+ console.log("Program Ended first");
+
+ 
+fs.readFile('input.txt', function (err, data) {
+   if (err) return console.error(err);
+   console.log(data.toString());
+});
+console.log("Program Ended two");
+
+
+var express = require('express');
+var app = express();
 app.set('port', 8880);
-
-app.get('/', function (req, res) {
-   res.send('Hello Madurai');
-   console.log("Cookies: ", req.cookies)
+app.post('/', function (req, res) {
+res.send('Hello Madurai');
 })
 
+app.get('/ab*cd', function(req, res) {   
+    console.log("Got a GET request for /ab*cd");
+    res.send('karthik Pattern Match');
+ })
 
 
-app.get('/index.htm', function (req, res) {
-   res.sendFile( __dirname + "/" + "index.htm" );
-})
-
-app.get('/process_get', function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.query.first_name,
-      last_name:req.query.last_name
-   };
-   console.log(response);
-   console.log(req.cookies);
-   res.send(JSON.stringify(response));
-})
-
-app.post('/process_post', urlencodedParser, function (req, res) {
-   // Prepare output in JSON format
-   response = {
-      first_name:req.body.first_name,
-      last_name:req.body.last_name
-   };
-   console.log(response);
-   console.log(req.cookies);
-   res.send(JSON.stringify(response));
-})
+ app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+ })
 
 
-app.post('/file_upload', function (req, res) {
-   console.log(req.file.name);
-   console.log(req.file.path);
-   console.log(req.file.type);
-   var file = __dirname + "/" + req.file.name;
-
-   // var tempPath = req.file.path, 
-   // ext = path.extname(req.file.name).toLowerCase(),
-   // targetPath = path.resolve('./tmp/' + file + ext);
-
-   // if (ext === '.png' || ext === '.jpg' || ext === '.jpeg' || ext === '.gif') {
-   //    fs.rename(tempPath, targetPath, function(err) {
-   //       if (err) throw err;
-   //       //res.redirect('/images/' + imgUrl);
-   //    });
-   // } else {
-   //    fs.unlink(tempPath, function () {
-   //       if (err) throw err;
-   //       //res.json(500, {error: 'Only image files are allowed.'});
-   //    });
-   // }
-
-   fs.readFile( req.file.path, function (err, data) {
-      fs.writeFile(file, data, function (err) {
-         if( err ) {
-            console.log( err );
-            } else {
-               response = {
-                  message:'File uploaded successfully',
-                  filename:req.file.name
-               };
-            }
-         console.log(req.cookies);
-         console.log( response );
-         res.end( JSON.stringify( response ) );
-      });
-   });
-})
-
-
-
+ app.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    response = {
+       first_name:req.query.first_name,
+       last_name:req.query.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+ })
+ 
+ app.post('/process_post', urlencodedParser, function (req, res) {
+    // Prepare output in JSON format
+    response = {
+       first_name:req.body.first_name,
+       last_name:req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
+ })
+ 
 
 var server = app.listen(app.get('port'), function() {
-   console.log('Express server listening on port ' + server.address().port); 
-})
+    console.log('Express server listening on port ' + server.address().port); 
+    })
+ 
+
+ 
